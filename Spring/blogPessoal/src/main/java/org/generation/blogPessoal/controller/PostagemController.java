ad7e36d942fa2f7c055d5612568e0f7f663b68ff
turma30java <felipe_matos@hotmail.com.br>
2021-09-02 01:59:48 -0300

    Controller

---
+++ Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,17 @@
+package org.generation.blogPessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.generation.blogPessoal.repository.PostagemRepository;
+
+@RestController 
+@RequestMapping ("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+
+	@Autowired 
+	private PostagemRepository repository;
+}
